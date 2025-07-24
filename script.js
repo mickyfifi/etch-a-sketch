109f316aedf4bb7b99c74748ffecef637a80e5c4
mickyfifi <94443260+mickyfifi@users.noreply.github.com>
2025-07-24 11:13:48 +0800

    Create Etch a sketch

---
+++ script.js
@@ -0,0 +1,50 @@
+const BOX_WIDTH = 27;
+let gridCount = 16;
+
+const containerElement = document.querySelector('#container');
+const setupButton = document.querySelector('#setup');
+
+setupButton.addEventListener('click', () => {
+    input = prompt('Number of grid?');
+    if (input > 100) {
+        alert('Maximum only of 100');
+        return;
+    }
+    gridCount = input;
+    setupNew(containerElement, gridCount);
+});
+
+function addGrid(containerElement, gridCount) {
+    containerElement.style.width = `${BOX_WIDTH * gridCount}px`;
+
+    for (let row = 0; row < gridCount; row++) {
+        for (let col = 0; col < gridCount; col++) {
+            const boxElement = document.createElement('div');
+            boxElement.id = `box-r${row + 1}-c${col + 1}`;
+            boxElement.classList.add('box');
+
+            boxElement.addEventListener('mouseover', () => {
+                if (boxElement.style.backgroundColor) {
+                    boxElement.style.opacity = Number(boxElement.style.opacity) + 0.10;
+                    return;
+                }
+
+                const red = Math.floor(Math.random() * 256);
+                const green = Math.floor(Math.random() * 256);
+                const blue = Math.floor(Math.random() * 256);
+
+                boxElement.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+                boxElement.style.opacity = 0.10;
+            });
+
+            containerElement.appendChild(boxElement);
+        }
+    }
+}
+
+function setupNew(containerElement, gridCount) {
+    containerElement.replaceChildren();
+    addGrid(containerElement, gridCount);
+}
+
+addGrid(containerElement, gridCount);
